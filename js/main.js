65e88d3258d3d35e005817de63a887ff97cd8d96
Isaiah Brashears <isaiah.brashears13@gmail.com>
2019-09-05 16:52:52 -0500

    .

--- js/main.js
+++ js/main.js
@@ -12,17 +12,8 @@
         $('.responsive-menu').stop(true,true).slideToggle();
         return false;
     });
-    $('.toggle-menu').on( 'tap',function(){
-        $('.responsive-menu').stop(true,true).slideToggle();
-        return false;
-    });
 
-    $('.project-inside').click(function () {
-        // var addressValue = $(this).children().children().attr("href");
-        // console.log(addressValue)
 
-        console.log('test')
-    })
 
 
 
