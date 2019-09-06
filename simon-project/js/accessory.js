f1e0c6b829d7a9047cad13e54df511c559acc653
Isaiah Brashears <isaiah.brashears13@gmail.com>
2019-09-06 08:46:52 -0500

    shortened time between lights simone

--- simon-project/js/accessory.js
+++ simon-project/js/accessory.js
@@ -2,26 +2,26 @@ function greenLight() {
     $('.top-left2').addClass('green');
     setTimeout(function () {
         $('.top-left2').removeClass('green');
-    },800)
+    },600)
 }
 
 function redLight() {
     $('.top-right2').addClass('red');
     setTimeout(function () {
         $('.top-right2').removeClass('red');
-    },800)
+    },600)
 }
 
 function yellowLight() {
     $('.bottom-left2').addClass('yellow');
     setTimeout(function () {
         $('.bottom-left2').removeClass('yellow');
-    },800)
+    },600)
 }
 
 function blueLight() {
     $('.bottom-right2').addClass('blue');
     setTimeout(function () {
         $('.bottom-right2').removeClass('blue');
-    },800)
+    },600)
 }
