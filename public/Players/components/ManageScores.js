f0cf0384dbbc4e0bd5755c50e55f77b45dd54e75
Isaiah Brashears <isaiah.brashears13@gmail.com>
2022-02-09 22:38:11 -0600

    updated project with webpacker

---
+++ public/Players/components/ManageScores.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { observer } from 'mobx-react-lite';
+
+const ManageScores = observer(function ManageScores({ store }) {
+
+  return (
+    <div>here's a div</div>
+  )
+});
+
+export default ManageScores;
