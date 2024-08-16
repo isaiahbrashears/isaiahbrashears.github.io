55ef90d4e5e371a94c52cb02e6d1cf9a2a0a6597
Isaiah Brashears <isaiah.brashears13@gmail.com>
2024-08-16 11:32:36 -0500

    updated outside of known answers cases

--- src/components/BeckWizard/steps/StepOne.js
+++ src/components/BeckWizard/steps/StepOne.js
@@ -54,6 +54,14 @@ const StepOne = ({ changeStep }) => {
         setShowInput(true);
       }, 48000);
       return () => clearTimeout(timer);
+    } else {
+      setDisplayText(
+        [
+          "No one knows what you're trying to say.", 2000,
+          "No one knows what you're trying to say. Let's try again", 2000,
+          'Would you like to play a game?'
+        ])
+      setKey(prevKey => prevKey + 1)
     }
   }
 
